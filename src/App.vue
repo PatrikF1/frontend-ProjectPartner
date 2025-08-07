466e0b5757b6918fd8e7ab9631fbd18093ae4c05
Patrik Fabijanic <patrik.fabijanic9@gmail.com>
2025-08-07 14:39:15 +0200

    pocetak projekta, setupiranje vue js-a i dodavanje jednostavnog vue routera na app.vue

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router';
+
+</script>
+
+<template>
+  <h1 class="naslov">Dobrodošli na Project Partner!</h1>
+  <RouterLink to="login">Prijava</RouterLink>
+  <RouterLink to="registration">Registracija</RouterLink>
+</template>
+
+<style scoped>
+
+.naslov {
+  text-align: center;
+}
+
+</style>
