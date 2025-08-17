15eca64f0b82aac6527892da4c4774794b542fc6
Patrik Fabijanic <patrik.fabijanic9@gmail.com>
2025-08-17 15:21:00 +0200

    probavanje dohvacanja korisnika sa backenda i koristenje typescripta

---
+++ backend.ts
@@ -0,0 +1,5 @@
+import axios from 'axios'
+
+export const backend = axios.create({
+  baseURL: 'http://localhost:https://backend-projectpartner-production.up.railway.app',
+})
--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
-<script setup lang="ts">
-import { RouterLink } from 'vue-router';
-
-</script>
-
 <template>
   <h1 class="naslov">Dobrodošli na Project Partner!</h1>
   <RouterLink to="login">Prijava</RouterLink>
   <RouterLink to="registration">Registracija</RouterLink>
 </template>
+
+<script setup lang="ts">
+import { RouterLink } from 'vue-router';
+
+</script>
 
 <style scoped>
 
