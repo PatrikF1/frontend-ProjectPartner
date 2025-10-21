366d52892df391effd3d1fbd6dd3985ee3720106
Patrik Fabijanic <patrik.fabijanic9@gmail.com>
2025-10-21 17:09:37 +0200

    tailwind nije radio nova verzija pa nove komponente ....

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,12 @@
 <template>
   <div class="p-6">
-    <Table />
+    <h1 class="text-3xl font-bold text-gray-900 dark:text-white mb-6">Welcome to ProjectPartner</h1>
+    <div class="bg-white dark:bg-gray-800 rounded-lg shadow-lg p-6">
+      <p class="text-gray-600 dark:text-gray-300">
+        Your dashboard is ready! Add your components here.
+      </p>
+    </div>
   </div>
 </template>
 
-<script setup lang="ts">
-import Table from '@/components/Table.vue'
-</script>
+<script setup lang="ts"></script>
--- tailwind.config.cjs
+++
@@ -1,8 +0,0 @@
-const { iconsPlugin, getIconCollections } = require('@egoist/tailwindcss-icons')
-
-module.exports = {
-  darkMode: 'class',
-  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  theme: { extend: {} },
-  plugins: [require('flyonui'), require('daisyui'), iconsPlugin({ collections: getIconCollections(['tabler']) })],
-}
